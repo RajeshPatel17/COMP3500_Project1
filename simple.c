9f4d778e660e5aa440bce15c77834762327ebbba
Rajesh Patel <rajesh.patel20110@gmail.com>
2021-08-21 15:41:59 -0500

    finished git script and added comment in simple.c for diff

--- simple.c
+++ simple.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	int array[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+	int array[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}; #comment for diff
 	double average = 0.00;
 	
 	for(int i = 0; i < 10; i++)
